0b545c3e89a9b3d4b1a218257ff87a9049ffc6ff
Kevin Coder <kingonyt4@gmail.com>
2021-11-28 12:19:05 -0800

    Added email user valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length: {minimum:3, maximum:105}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i;
+    validates :email, presence: true, length: {minimum:3, maxium: 105}, format: { with: VALID_EMAIL_REGEX };
+    
 end
